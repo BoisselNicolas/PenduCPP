12dc767563d5e038e3086424a40a7eed5e067864
BoisselNicolas <ncboissel@gmail.com>
2020-10-16 21:39:00 +0200

    Correction + Ajout de la règle de victoire.

--- main.cpp
+++ main.cpp
@@ -5,57 +5,65 @@ using namespace std;
 string inputWord;
 string inputLetter;
 int size = 0;
+int sizeTwo = 0;
 string str;
 bool inf = false;
 bool result = false;
 int indexSubstr = 0;
 int indexSubstrTwo = 1;
+int score = 0;
 
-
-void drawTheWord();
 
 int main(){
 
+    cout << " Veuillez saisir un mot. " << endl ;
+    cin >> inputWord ;
+    cout << endl ;
+
+    size = inputWord.size();
+    size = size + 1;
+    sizeTwo = size - 1;
+
+    string letterArray[size];
+    string hiddenWord[size];
+    
+    cout << endl;
+
+    for (size_t i = 0; i < sizeTwo; i++)
+    {
+        letterArray[i] = inputWord[i];
+        hiddenWord[i] = "*";
+        cout << hiddenWord[i];
+    }
+
+    cout << endl;
+
     while(result == false){
+        inputLetter = "";
         
-        cout << " Veuillez saisir un mot. " << endl ;
-        cin >> inputWord ;
-        cout << endl ;
-
-        size = inputWord.size();
-        size = size + 1;
-
-        string letterArray[size];
-        
-        cout << endl;
-        for (size_t i = 0; i != size; i++)
-        {
-            if(inf == false){
-                letterArray[i] = inputWord.substr (indexSubstr,indexSubstrTwo);
-                indexSubstr ++;
-                inf = true ;
-            }else{
-                letterArray[i] = inputWord.substr (indexSubstr,indexSubstrTwo);
-                indexSubstr ++;
-                indexSubstrTwo ++;
-            }
-        };
-        
-        cout << " Choisissez une lettre " ;
+        cout << "Choisissez une lettre :" ;
         cin >> inputLetter ;
         cout << endl;
 
-        for (size_t i = 0; i < size; i++)
+        for (size_t i = 0; i < sizeTwo; i++)
         {
             if(letterArray[i] == inputLetter){
-                cout << "Tu as trouvé une lettre ! " << endl;
+                
+                hiddenWord[i] = letterArray[i];
+                score = score + 1;
+                
             }
+            
+            cout << hiddenWord[i] ;
+        }
+       cout << endl ;
+
+       if(score == sizeTwo)
+       {
+            result = true;
         }
     }
-    
-    
 
-
-    return 0;
+    cout << "FELICITATION !" << endl;
 }
 
